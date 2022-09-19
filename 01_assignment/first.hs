566031cd6360357f07bc219d1656615489356e3c
Yannick Huggler <yannick.huggler@polypoint.ch>
2022-09-19 13:16:32 +0200

    Implemented assignment 1

---
+++ 01_assignment/first.hs
@@ -0,0 +1,18 @@
+{-
+Das erste Haskell Script
+-}
+
+size :: Integer
+size = (4 - 3) * 12
+
+-- Gibt den doppelten Wert zurück
+times2 :: Integer -> Integer
+times2 x = 2 * x                -- (t2)
+
+-- Quadriert den Wert
+square :: Integer -> Integer
+square x = x * x               -- (sqr)
+
+-- Pythagoras a^2 + b^2 = c^2
+pyth :: Integer -> Integer -> Integer
+pyth a b = square a + square b  -- (py)
